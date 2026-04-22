0a02233ff2b0c84d12154390b960e5c7a5476c25
Yulia Sharabi <yudit77091@gmail.com>
2026-04-22 15:44:06 +0400

    chore: fix environment configuration and typescript support

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,20 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsparser from '@typescript-eslint/parser'
 
 export default [
   { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: tsparser,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -20,14 +26,20 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@typescript-eslint': tseslint,
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      // Allow 'React' to be imported but not explicitly used in code
+      '@typescript-eslint/no-unused-vars': ['error', { 'varsIgnorePattern': '^React$' }],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
     },
   },
 ]
