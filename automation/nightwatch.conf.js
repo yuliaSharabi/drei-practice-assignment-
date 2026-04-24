267a25d9ada4b55a6bbb8785d996efc60987e767
Yulia Sharabi <yudit77091@gmail.com>
2026-04-24 13:09:36 +0400

    refactor: optimize Nightwatch webdriver configuration structure

--- automation/nightwatch.conf.js
+++ automation/nightwatch.conf.js
@@ -4,15 +4,12 @@ module.exports = {
   webdriver: {
     start_process: true,
     server_path: '',
-    port: 9515,
-    cli_args: ['--port=9515'],
     default_path_prefix: ''
   },
   test_settings: {
     default: {
       launch_url: 'http://localhost:3000',
       webdriver: {
-        start_process: true,
         server_path: 'node_modules/.bin/chromedriver',
         port: 9515
       },
